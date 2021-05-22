7bdec1f718cbc4ade37bb87b738f15fd250d5832
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2021-05-22 14:02:26 +0300

    Fixed serializers.

--- DatingAppProject/DatingApp/serializers.py
+++ DatingAppProject/DatingApp/serializers.py
@@ -1,37 +1,59 @@
 from rest_framework import serializers
 import DatingApp.models as models
+from DatingApp import services
 
 
+# Country -------------------------------------------------
 class CountrySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Country
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
+# City ----------------------------------------------------
 class CitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.City
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
+# Interest-------------------------------------------------
+class InterestSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Interest
+        fields = ('id', 'name')
+
+
+# User ----------------------------------------------------
 class PhotoForUserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = models.PhotoForUser
-        fields = '__all__'
+        fields = ('id', 'file_name', 'position')
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    photos = PhotoForUserSerializer(many=True)
+    photos = serializers.SerializerMethodField()
+
+    def get_photos(self, obj):
+        photos_query_set = services.get_photos(obj.id)
+        photos = map(lambda photo: PhotoForUserSerializer(photo).data, photos_query_set)
+        return photos
 
     class Meta:
         model = models.CustomUser
-        fields = '__all__'
+        fields = ('id', 'first_name', 'second_name',
+                  'email', 'phone_number', 'photos',
+                  'gender', 'city',
+                  'birthdate', 'information')
 
 
 class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.CustomUser
-        fields = '__all__'
+        fields = ('id', 'first_name', 'second_name',
+                  'email', 'gender', 'city')
